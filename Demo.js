91f1b0fe6c4b3485fea0a0c3d3e6529ce27b1d36
HAMIDI Liesse <LHAMID2I@home.helpline.fr>
2020-09-14 13:57:16 +0200

    first commit

---
+++ Demo.js
@@ -0,0 +1,78 @@
+﻿let TabParam = ["MsgBibli", "MsgDyn", "Msgbiblicolor"];
+let TabGroupe1 = ["1", "2", "3"];
+var aff = TabParam;
+var infos = {
+    Donnee1: 3,
+    Donnee2: 4,
+    Donnee3: 1,
+    Donnee4: 0
+}
+
+
+function efftable() {
+
+    document.getElementById('tab').innerHTML = "";
+
+}
+
+function creerTable1() {
+    efftable();
+    let div = document.getElementById('tab');
+    var table = document.createElement("table");
+    var ligne = table.insertRow(0);
+    var cellule1 = ligne.insertCell(0);
+    var cellule2 = ligne.insertCell(1);
+    cellule1.innerHTML = infos.Donnee1;
+    cellule2.innerHTML = infos.Donnee2;
+    div.append(table);
+}
+
+function creerTable2() {
+    efftable();
+    let div = document.getElementById('tab');
+    var table = document.createElement("table");
+    var ligne = table.insertRow(0);
+    var cellule1 = ligne.insertCell(0);
+    var cellule2 = ligne.insertCell(1);
+    cellule1.innerHTML = infos.Donnee3;
+    cellule2.innerHTML = infos.Donnee4;
+    div.append(table);
+}
+
+function choixTab() {
+    let choix = document.getElementById('menugroup').value;
+    console.log("c'est le " + choix);
+    if (choix == "G1") {
+        creerTable1();
+    }
+    else {
+        creerTable2();
+    }
+
+}
+
+function msgapply() {
+    var message = document.getElementById('msg');
+    message.innerHTML ="Modifications applied with success !";
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
